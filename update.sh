690fea69bbd63c9e4e13ad65468a5ce615f2a49b
theevocater <theevocater@gmail.com>
2011-12-02 15:43:26 -0500

    updating my update script (thanks to wanderson) and pushing new updates to my submodules.

--- update.sh
+++ update.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# stolen from the lovely demarko
-# https://github.com/DeMarko/dotvim/blob/master/update.sh
 
-txtgrn='\033[1;32m' # Green
-txtrst='\033[0m' # Text Reset
+ git submodule foreach --recursive git submodule update
 
-cd $HOME/.vim/bundle/
-for bundle in *
-do
-cd $bundle
-    echo -e "${txtgrn}${bundle}${txtrst}"
-    git pull origin master
-    cd ..
-done
