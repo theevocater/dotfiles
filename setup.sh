ed2b6f6b02455fe9442c2b5aa6ca1b68dcfa3057
Jake Kaufman <theevocater@gmail.com>
2012-12-26 19:09:14 -0800

    changing git-completion.bash to be downloaded
    it was definitely getting out of date so instead ill just download the file w/
    the setup script.

--- setup.sh
+++ setup.sh
@@ -8,18 +8,25 @@ cd -P "$( dirname "$0" )"
 # distribution
 for file in bash_logout bash_profile bashrc gitconfig gvimrc inputrc tmux.conf screenrc vim vimrc cvsignore
 do
-    # If the file exists, ask the user if they'd like us to move it to
-    # FILENAME_old. Otherwise, overwrite.
-    if [[ -e ~/.${file} ]] ; then
-        read -p "~/.$file exists, overwrite? y[n] " -n 1
-        echo
-        # if the answer isn't yes, skip
-        if [[ -z $REPLY || $REPLY =~ ^[^Yy]$ ]] ; then
-            continue
-        fi
+  # If the file exists, ask the user if they'd like us to move it to
+  # FILENAME_old. Otherwise, overwrite.
+  if [[ -e ~/.${file} ]] ; then
+    read -p "~/.$file exists, overwrite? y[n] " -n 1
+    echo
+    # if the answer isn't yes, skip
+    if [[ -z $REPLY || $REPLY =~ ^[^Yy]$ ]] ; then
+      continue
     fi
-    # Add the appropriate symlink
-    ln -svnf ${PWD}/${file} ~/.${file}
+  fi
+  # Add the appropriate symlink
+  ln -svnf ${PWD}/${file} ~/.${file}
 done
 
+echo "Downloading git completion script"
+if [[ !(-f git-completion.bash) ]]
+then
+  curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
+fi
+
+echo "Syncing submodules"
 ./sync-sb.sh
