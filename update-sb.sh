adab30fc657f3f1281086a28cb8d105b21c31478
Jake Kaufman <theevocater@gmail.com>
2013-06-03 22:19:47 +0000

    Updating update-sb.sh

--- update-sb.sh
+++ update-sb.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull
-
-#git submodule update
+git submodule foreach --recursive git pull origin master
