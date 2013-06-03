0cd753c4d988ec74930003967f3459bca702beb0
Jake Kaufman <theevocater@gmail.com>
2013-06-03 22:38:23 +0000

    Update setup.sh to fetch "correct" version of git

--- setup.sh
+++ setup.sh
@@ -15,6 +15,14 @@ prompt () {
   return 0
 }
 
+download_git () {
+  git_version=`git --version | cut -d" " -f3`
+
+  echo "Downloading git completion script for $git_version"
+  curl https://raw.github.com/git/git/v$git_version/contrib/completion/git-completion.bash > git-completion.bash
+  curl https://raw.github.com/git/git/v$git_version/contrib/completion/git-prompt.sh > git-prompt.sh
+}
+
 # Iterate over the list of setup files we want to alias from our dotfile
 # distribution
 for file in bash_logout bash_profile bashrc gitconfig gvimrc inputrc tmux.conf screenrc vim vimrc cvsignore
@@ -39,15 +47,12 @@ done
 
 if [[ !(-f git-completion.bash) ]]
 then
-  echo "Downloading git completion script"
-  curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
-  curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
+  download_git
 else
   prompt "Update completion?"
   if [[ $? -eq 0 ]]
   then
-    curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
-    curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
+    download_git
   fi
 fi
 
