f8741e21f6b52b1d228e0da58115a3e26daeb087
Jake Kaufman <me@jake.computer>
2019-05-28 01:20:15 -0400

    don't reinstall rustup

--- setup.d/20-rust.sh
+++ setup.d/20-rust.sh
@@ -2,6 +2,10 @@
 set -eu -o pipefail
 # install rust
 # TODO(jakeman) there has to be a better way
-echo "Installing rust"
 
+if command -v rustup &>/dev/null ; then
+  exit 0
+fi
+
+echo "Installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
