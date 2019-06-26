cb5a47fce5a77a110c0f1da4c7a18f16f87d1136
Jake Kaufman <me@jake.computer>
2019-06-26 00:47:14 -0400

    add basic docker installer for li nu x

---
+++ setup.d/03-docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu -o pipefail
+# install docker-ce
+
+sudo apt update
+sudo apt install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg > /tmp/docker.pub
+
+expected='9DC858229FC7DD38854AE2D88D81803C0EBFCD88'
+fingerprint=$(gpg --with-fingerprint --import-options show-only --import < /tmp/docker.pub | head -n2 | tail -n1 | tr -d ' ')
+
+if [[ "$expected" != "$fingerprint" ]] ; then
+  echo "Warning fingerprint for docker key is wrong"
+  exit 1
+fi
+
+sudo apt-key add /tmp/docker.pub
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable edge test"
+
+sudo apt update
+sudo apt install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io
+
+sudo usermod -aG docker "${USER}"
