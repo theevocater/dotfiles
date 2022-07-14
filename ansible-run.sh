4be7553cea3002346b5b1154687198491fe8dba9
Jake Kaufman <me@jake.computer>
2022-07-14 15:47:12 -0400

    Update some ansible tasks
    * Simplify the ansible-run script
    * Add vim, python tags to make it easy to just run those tasks
    * Remove bootstrap script

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 set -eu -o pipefail
 
+
+bootstrap() {
+  sync_submodules() {
+    git submodule sync --recursive
+    git submodule update --init --recursive
+  }
+
+  install_homebrew() {
+    if ! command -v brew &>/dev/null; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    if [[ -d /opt/homebrew/bin ]] ; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+    brew upgrade
+    brew install ansible
+  }
+
+  setup_apt() {
+    apt update
+    apt install -y ansible
+  }
+
+  sync_submodules
+
+  if [[ "$(uname -s)" =~ "Darwin" ]]; then
+    install_homebrew
+  elif [[ "$(lsb_release -s -i)" =~ "Ubuntu" ]]; then
+    sudo sh -c setup_apt
+  else
+    echo "Unable to determine base OS. Not installing"
+    return 1
+  fi
+}
+
 # install ansible if necessary
-if ! command -v  ansible ; then
-  scripts/bootstrap-ansible.sh
+if ! command -v  ansible &>/dev/null ; then
+  bootstrap
 fi
 
 # Run playbooks that require root
-ansible-playbook --verbose --inventory ansible/hosts --ask-become-pass ansible/root.yml
+ansible-playbook --verbose --inventory ansible/localhost --ask-become-pass ansible/root.yml
 
 # Run playbooks that are for me
-ansible-playbook --verbose --inventory ansible/hosts ansible/jakeman.yml
+ansible-playbook --verbose --inventory ansible/localhost ansible/jakeman.yml
