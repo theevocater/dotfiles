40408665f62c8a06dd32c14fdabb1dfa7447125b
Jake Kaufman <me@jake.computer>
2022-09-02 12:37:48 -0400

    Updates!

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OS=$(uname)
 submodules() {
   echo "Updating git submodules to master"
   git submodule foreach git pull origin master
@@ -13,16 +14,20 @@ python_packages() {
 }
 
 precommit() {
-  echo "epdating pre-commit hooks"
+  echo "updating pre-commit hooks"
   pre-commit autoupdate
 }
 
 moom() {
+  if [[ "$OS" != "Darwin" ]] ; then
+    return 0
+  fi
   echo "Dumping current moom preferences"
   scripts/moom-save.sh plists/Moom.plist
 }
 
 # TODO(jakeman) persist plists/Solarized Jake.terminal as well
+#u
 
 submodules
 python_packages
