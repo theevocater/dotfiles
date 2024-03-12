55c8719b8a7f634029e0225d492b968b27c556c8
Jake Kaufman <me@jake.computer>
2024-03-12 00:14:56 -0400

    Add my own snippets package

--- rc.d/nvim/init.lua
+++ rc.d/nvim/init.lua
@@ -482,6 +482,7 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load({ paths = './snippets' })
 luasnip.config.setup {}
 
 cmp.setup {
