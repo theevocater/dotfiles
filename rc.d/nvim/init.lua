85042b6238abaa12a5bf19274617fc2c356dc3b2
Jake Kaufman <me@jake.computer>
2024-05-15 08:53:30 -0400

    Add menufacture and oil

--- rc.d/nvim/init.lua
+++ rc.d/nvim/init.lua
@@ -136,7 +136,8 @@ require('lazy').setup({
           return vim.fn.executable 'make' == 1
         end,
       },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
+      'nvim-telescope/telescope-ui-select.nvim',
+      'molecule-man/telescope-menufacture',
     },
     config = function()
       -- Setup Telescope
@@ -150,14 +151,19 @@ require('lazy').setup({
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'menufacture')
+
+      -- menufacture overrides
+      local menufacture = require('telescope').extensions.menufacture
+      vim.keymap.set('n', '<leader>sf', menufacture.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sw', menufacture.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', menufacture.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>gf', menufacture.git_files, { desc = 'Search by [G]it [F]iles' })
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -175,6 +181,7 @@ require('lazy').setup({
   },
 
   'github/copilot.vim',
+  { 'stevearc/oil.nvim', opts = {} },
 })
 
 -------------------------------------------------------------------------------
