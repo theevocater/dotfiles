c0843d9b65ca80e310fd6cf5511467a9d0308341
Jake Kaufman <me@jake.computer>
2024-05-15 08:18:12 -0400

    Add some motions for telescope

--- rc.d/nvim/init.lua
+++ rc.d/nvim/init.lua
@@ -138,7 +138,33 @@ require('lazy').setup({
           return vim.fn.executable 'make' == 1
         end,
       },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
     },
+    config = function()
+      -- Setup Telescope
+      require('telescope').setup {
+        extensions = {
+          ['ui_select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
+      }
+      -- Enable telescope fzf native, if installed
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    end
   },
 
   {
@@ -329,16 +355,6 @@ vim.keymap.set('n', '<leader>c', ':%s/\\s\\+$//<CR>', { silent = true, })
 -- Setup fugitive
 -- open fugitive Git status window
 vim.keymap.set('n', '<leader>g', ':Git<CR>', { silent = true, })
-
-
--- Setup Telescope
-require('telescope').setup {}
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- TODO read telescope docs / kickstart to use more stuff here
-vim.keymap.set('n', '<leader>t', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-
 
 -- Setup Treesitter
 -- See `:help nvim-treesitter`
