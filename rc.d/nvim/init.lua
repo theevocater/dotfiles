e45053fdb0cc3fb3a5dfa4e3006bb769b74ed19d
Jake Kaufman <me@jake.computer>
2025-04-28 17:52:04 -0400

    Update
    Update generally
    
    Fix a few things to work better with neovim .11.1.

--- rc.d/nvim/init.lua
+++ rc.d/nvim/init.lua
@@ -307,6 +307,13 @@ vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
   end,
 })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = {
+    current_line = true,
+  },
+})
+
 -- see if any files have changed when switching buffers
 vim.api.nvim_create_autocmd({ 'WinEnter' }, {
   pattern = { '*' },
@@ -523,33 +530,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --  To jump back, press <C-t>.
     map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-    -- Find references for the word under your cursor.
-    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-    -- Jump to the implementation of the word under your cursor.
-    --  Useful when your language has ways of declaring types without an actual implementation.
-    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
     -- Jump to the type of the word under your cursor.
     --  Useful when you're not sure what type a variable is and you want to see
     --  the definition of its *type*, not where it was *defined*.
     map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
-    -- Fuzzy find all the symbols in your current document.
-    --  Symbols are things like variables, functions, types, etc.
-    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
     -- Fuzzy find all the symbols in your current workspace.
     --  Similar to document symbols, except searches over your entire project.
     map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- Rename the variable under your cursor.
-    --  Most Language Servers support renaming across files, etc.
-    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-    -- Execute a code action, usually your cursor needs to be on top of an error
-    -- or a suggestion from your LSP for this to activate.
-    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
     -- Opens a popup that displays documentation about the word under your cursor
     --  See `:help K` for why this keymap.
