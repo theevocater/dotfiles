1228d0316ef9d2c225c8ee5e704bcbab6c9e929c
Jake Kaufman <me@jake.computer>
2025-05-31 12:54:03 -0400

    Update (#170)
    - **New colorschemes**
    - **Update**

--- rc.d/nvim/init.lua
+++ rc.d/nvim/init.lua
@@ -23,16 +23,83 @@ require("lazy").setup({
 		opts = {
 			fallback = "light",
 		},
+		config = function()
+			vim.cmd.colorscheme("kanagawa")
+		end,
 		dependencies = {
 			{
 				"Tsuzat/NeoSolarized.nvim",
 				opts = {
-					style = "light",
 					transparent = false,
 				},
-				config = function()
-					vim.cmd.colorscheme("NeoSolarized")
-				end,
+			},
+			{
+				"rebelot/kanagawa.nvim",
+				opts = {
+					compile = false, -- enable compiling the colorscheme
+					undercurl = true, -- enable undercurls
+					commentStyle = { italic = true },
+					functionStyle = {},
+					keywordStyle = { italic = true },
+					statementStyle = { bold = true },
+					typeStyle = {},
+					transparent = false, -- do not set background color
+					dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+					terminalColors = true, -- define vim.g.terminal_color_{0,17}
+					colors = { -- add/modify theme and palette colors
+						palette = {
+							-- Reduce the amount of yellow in the white
+							-- lotusWhite0 = "#d5cea3",
+							lotusWhite0 = "#dfdeb3",
+							-- lotusWhite1 = "#dcd5ac",
+							lotusWhite1 = "#dcd5ac",
+							-- lotusWhite2 = "#e5ddb0",
+							lotusWhite2 = "#e5ddb0",
+							-- lotusWhite3 = "#f2ecbc",
+							lotusWhite3 = "#fdf6e3",
+							-- lotusWhite4 = "#e7dba0",
+							lotusWhite4 = "#eee8d5",
+							-- lotusWhite5 = "#e4d794",
+							lotusWhite5 = "#e9e3c5",
+						},
+						theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+					},
+					overrides = function(colors) -- add/modify highlights
+						return {}
+					end,
+					theme = "lotus",
+					background = {
+						dark = "wave",
+						light = "lotus",
+					},
+				},
+			},
+			{
+				"EdenEast/nightfox.nvim",
+				opts = {
+					-- options = {},
+					-- palettes = {},
+					-- specs = {},
+					-- groups = {},
+				},
+			},
+			{ "ellisonleao/gruvbox.nvim" },
+			{
+				"rose-pine/neovim",
+				name = "rose-pine",
+			},
+			{
+				"catppuccin/nvim",
+				name = "catppuccin",
+				opts = {
+					background = { light = "latte", dark = "mocha" },
+				},
+			},
+			{ "nuvic/flexoki-nvim", name = "flexoki" },
+
+			{
+				"folke/tokyonight.nvim",
+				opts = {},
 			},
 		},
 	},
@@ -162,7 +229,7 @@ require("lazy").setup({
 			signs = {
 				add = { text = "+" },
 				change = { text = "~" },
-				delete = { text = "_" },
+				delete = { text = "-" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
